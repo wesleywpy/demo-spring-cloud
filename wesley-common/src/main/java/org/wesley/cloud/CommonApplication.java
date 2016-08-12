de76e7c5a25368a78dd90e2a44dca68c0d8d4b10
Wesley <wangp_yong@163.com>
2016-08-12 13:25:27 +0800

    common module 通用模块

---
+++ wesley-common/src/main/java/org/wesley/cloud/CommonApplication.java
@@ -0,0 +1,18 @@
+package org.wesley.cloud;
+
+/**
+ * @author Created by Wesley on 2016/8/11.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommonApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(CommonApplication.class, args);
+    }
+}
