24447516f50e6e2cbe2e63c01c20f5bc8ab05d46
Wesley <wangp_yong@163.com>
2016-10-24 15:10:28 +0800

    分布式配置中心

--- wesley-config-server/src/main/java/org/wesley/cloud/ConfigServerApp.java
+++ wesley-config-server/src/main/java/org/wesley/cloud/ConfigServerApp.java
@@ -2,19 +2,19 @@ package org.wesley.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 分布式配置中心
  * @author Created by Wesley on 2016/9/12.
  */
-@SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApp {
 
     public static void main(String[] args){
-        new SpringApplicationBuilder(ConfigServerApp.class).web(true).run(args);
+        SpringApplication.run(ConfigServerApp.class, args);
     }
 }
