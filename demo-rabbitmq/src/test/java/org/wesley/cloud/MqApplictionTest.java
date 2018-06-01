89059d9c72b4f1167f2997686a4efcfd770e74cc
Wesley <wangp_yong@163.com>
2018-06-01 17:43:23 +0800

    rabbitmq测试

--- demo-rabbitmq/src/test/java/org/wesley/cloud/MqApplictionTest.java
+++ demo-rabbitmq/src/test/java/org/wesley/cloud/MqApplictionTest.java
@@ -1,10 +1,9 @@
 package org.wesley.cloud;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.wesley.cloud.jms.Sender;
 
@@ -12,7 +11,7 @@ import org.wesley.cloud.jms.Sender;
  * @author Created by Wesley on 2016/10/17.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MqAppliction.class)
+@SpringBootTest(classes = MqAppliction.class)
 public class MqApplictionTest {
 
     @Autowired
