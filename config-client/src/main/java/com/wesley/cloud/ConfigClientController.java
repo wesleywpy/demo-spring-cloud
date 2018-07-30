5067cd21605c22209ff2291d8198cbd91d2fca36
wesley <wangp_yong@163.com>
2018-07-30 22:41:16 +0800

    spring cloud config

---
+++ config-client/src/main/java/com/wesley/cloud/ConfigClientController.java
@@ -0,0 +1,17 @@
+package com.wesley.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+  @Value("${spring.application.name}")
+  private String profile;
+
+  @GetMapping("/app-name")
+  public String getProfile() {
+    return this.profile;
+  }
+}
--- config-server/src/main/java/org/wesley/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/org/wesley/cloud/ConfigServerApplication.java
@@ -3,18 +3,17 @@ package org.wesley.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 分布式配置中心
  * @author Created by Wesley on 2016/9/12.
  */
-@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApp {
+public class ConfigServerApplication {
 
     public static void main(String[] args){
-        SpringApplication.run(ConfigServerApp.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
+
 }
