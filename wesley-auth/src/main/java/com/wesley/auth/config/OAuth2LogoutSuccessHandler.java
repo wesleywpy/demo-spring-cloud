536b2f5904d0f94d259134c5218838c915fa89d0
yani <wangp_yong@163.com>
2019-12-27 18:34:53 +0800

    OAuth2 认证服务器 v3

---
+++ wesley-auth/src/main/java/com/wesley/auth/config/OAuth2LogoutSuccessHandler.java
@@ -0,0 +1,29 @@
+package com.wesley.auth.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 退出登录成功处理
+ * @author jojo
+ */
+@Component
+public class OAuth2LogoutSuccessHandler implements LogoutSuccessHandler {
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		String redirectUri = request.getParameter("redirect_uri");
+		if(!StringUtils.isEmpty(redirectUri)) {
+			response.sendRedirect(redirectUri);
+		}
+	}
+
+}
