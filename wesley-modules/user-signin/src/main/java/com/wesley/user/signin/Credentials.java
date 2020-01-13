a44eab48ed5461c95a60cd8382179a4023ed85da
yani <wangp_yong@163.com>
2020-01-13 16:12:06 +0800

    feat:基于OAuth2的登录模块

---
+++ wesley-modules/user-signin/src/main/java/com/wesley/user/signin/Credentials.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.wesley.user.signin;
+
+import lombok.Data;
+
+/**
+ * @author jojo
+ *
+ */
+@Data
+public class Credentials {
+
+	private String username;
+	
+	private String password;
+	
+}
